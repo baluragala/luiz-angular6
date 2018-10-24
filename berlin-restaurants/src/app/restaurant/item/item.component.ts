7e7ee769424d5838f037cd5b28acd15419c0a95d
Bala Krishna Ragala <baluragala@gmail.com>
2018-10-24 13:43:41 +0530

    day 3 - property binding (parent to child)

--- berlin-restaurants/src/app/restaurant/item/item.component.ts
+++ berlin-restaurants/src/app/restaurant/item/item.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'blr-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  selector: "blr-item",
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.css"]
 })
 export class ItemComponent implements OnInit {
+  // {
+  //   "id": 101004,
+  //   "name": "La Dolce Vita",
+  //   "address": "Waltersdorfer Straße 1A, Berlin, Germany",
+  //   "category": "restaurant",
+  //   "location": "Berlin",
+  //   "lat": 52.405091,
+  //   "lng": 13.57337,
+  //   "details": "http://tour-pedia.org/api/getPlaceDetails?id=101004"
+  //   }
 
-  constructor() { }
+  @Input()
+  restaurant = {
+    id: 101004,
+    name: "La Dolce Vita",
+    address: "Waltersdorfer Straße 1A, Berlin, Germany",
+    category: "restaurant",
+    location: "Berlin",
+    lat: 52.405091,
+    lng: 13.57337,
+    details: "http://tour-pedia.org/api/getPlaceDetails?id=101004"
+  };
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- berlin-restaurants/src/app/restaurant/list/list.component.ts
+++ berlin-restaurants/src/app/restaurant/list/list.component.ts
@@ -7,11 +7,59 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ListComponent implements OnInit {
   type: string = "Top 10";
+  restaurants = [
+    {
+      id: 101004,
+      name: "La Dolce Vita",
+      address: "Waltersdorfer Stra\u00dfe 1A, Berlin, Germany",
+      category: "restaurant",
+      location: "Berlin",
+      lat: 52.405091,
+      lng: 13.57337,
+      details: "http://tour-pedia.org/api/getPlaceDetails?id=101004"
+    },
+    {
+      address: "",
+      category: "restaurant",
+      id: 100263,
+      lat: 52.442259616828,
+      lng: 13.240633765583,
+      location: "Berlin",
+      name: "La Dolce Vita",
+      originalId: "4e209cb718a88345f0429983",
+      polarity: 0,
+      subCategory: "Italian Restaurant",
+      details: "http://tour-pedia.org/api/getPlaceDetails?id=100263"
+    },
+    {
+      id: 324915,
+      name: "La Dolce Vita",
+      address: "Wilsnacker Stra\u00dfe 62, Berlin, Germany",
+      category: "restaurant",
+      location: "Berlin",
+      lat: 52.527308,
+      lng: 13.351412,
+      numReviews: 1,
+      reviews: "http://tour-pedia.org/api/getReviewsByPlaceId?placeId=324915",
+      polarity: 0,
+      details: "http://tour-pedia.org/api/getPlaceDetails?id=324915"
+    }
+  ];
   constructor() {}
 
   ngOnInit() {}
 
   getType() {
     return this.type;
+  }
+
+  onClick(e) {
+    console.log(e);
+    alert("clicked...");
+  }
+
+  onMouseOver(e) {
+    console.log(e);
+    console.log("onMouseOver");
   }
 }
