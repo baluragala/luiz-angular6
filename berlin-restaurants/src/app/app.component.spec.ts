b994e8d01016e5d323918093481185c2e66c6271
Bala Krishna Ragala <baluragala@gmail.com>
2018-11-01 13:45:33 +0530

    unit testing - 1

--- berlin-restaurants/src/app/app.component.spec.ts
+++ berlin-restaurants/src/app/app.component.spec.ts
@@ -1,31 +1,39 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { RouterModule } from "@angular/router";
+import { APP_BASE_HREF } from "@angular/common";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let fixture, app;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [SharedModule, RouterModule.forRoot([])],
+      providers: [{ provide: APP_BASE_HREF, useValue: "" }]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  it("should create the app", () => {
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'berlin-restaurants'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('berlin-restaurants');
+    expect(app.title).toEqual("berlin-restaurants");
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it("should render title in a h1 tag", () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to berlin-restaurants!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to berlin-restaurants!"
+    );
+    app.title = "angular6";
+    fixture.detectChanges();
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to angular6!"
+    );
   });
 });
